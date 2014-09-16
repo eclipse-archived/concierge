2bb27dafc277a177bbc69283054eaeb7af0312fb
Jochen Hiller <j.hiller@telekom.de>
2014-09-16 22:59:45 +0200

    Added supporting bundles to Concierge * service.xmlparser (using OSGi XML Parser Spec) * extension.permission (OSGi permissionadmin and condpermadmin) * fixed build.xml to include build all bundles
    Signed-off-by: Jochen Hiller <j.hiller@telekom.de>

---
+++ org.eclipse.concierge.service.xmlparser/src/org/eclipse/concierge/service/xmlparser/internal/Activator.java
@@ -0,0 +1,11 @@
+package org.eclipse.concierge.service.xmlparser.internal;
+
+import org.osgi.util.xml.XMLParserActivator;
+
+/**
+ * This class provides extensibility of way how XML parsers will be registered.
+ * Actually the default behavior is OK, so nothing to add here.
+ */
+public class Activator extends XMLParserActivator {
+
+}
