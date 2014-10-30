de3cc7498486a414a72a46d7f96c8781717ac3eb
Jochen Hiller <j.hiller@telekom.de>
2014-10-30 22:05:22 +0100

    Added tests for installing framework extensions. Added tests for launching framework with different arguments.
    Signed-off-by: Jochen Hiller <j.hiller@telekom.de>

---
+++ org.eclipse.concierge/test/org/eclipse/concierge/BundlesWithFrameworkExtensionsTest.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jochen Hiller
+ *******************************************************************************/
+package org.eclipse.concierge;
+
+import org.eclipse.concierge.test.util.AbstractConciergeTestCase;
+import org.eclipse.concierge.test.util.SyntheticBundleBuilder;
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+
+/**
+ * @author Jochen Hiller - Initial Contribution
+ */
+public class BundlesWithFrameworkExtensionsTest extends
+		AbstractConciergeTestCase {
+
+	/**
+	 * This test will install a fragment bundle to framework
+	 * org.eclipse.concierge.
+	 */
+	@Test
+	public void testFrameworkExtensionFragmentOfConcierge() throws Exception {
+		try {
+			startFramework();
+			SyntheticBundleBuilder builder = SyntheticBundleBuilder
+					.newBuilder();
+			builder.bundleSymbolicName(
+					"testFrameworkExtensionFragmentOfConcierge")
+					.addManifestHeader("Bundle-Version", "1.0.0")
+					.addManifestHeader("Fragment-Host",
+							"org.eclipse.concierge; extension:=framework");
+			final Bundle bundleUnderTest = installBundle(builder);
+			enforceResolveBundle(bundleUnderTest);
+			assertBundleResolved(bundleUnderTest);
+		} finally {
+			stopFramework();
+		}
+	}
+
+	/**
+	 * This test will install a fragment bundle to framework system.bundle.
+	 */
+	@Test
+	public void testFrameworkExtensionFragmentOfSystemBundle() throws Exception {
+		try {
+			startFramework();
+			SyntheticBundleBuilder builder = SyntheticBundleBuilder
+					.newBuilder();
+			builder.bundleSymbolicName(
+					"testFrameworkExtensionFragmentOfSystemBundle")
+					.addManifestHeader("Bundle-Version", "1.0.0")
+					.addManifestHeader("Fragment-Host",
+							"system.bundle; extension:=framework");
+			final Bundle bundleUnderTest = installBundle(builder);
+			enforceResolveBundle(bundleUnderTest);
+			assertBundleResolved(bundleUnderTest);
+		} finally {
+			stopFramework();
+		}
+	}
+
+}
---
+++ org.eclipse.concierge/test/org/eclipse/concierge/FrameworkLaunchArgsTest.java
@@ -0,0 +1,155 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jochen Hiller
+ *******************************************************************************/
+package org.eclipse.concierge;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.concierge.test.util.AbstractConciergeTestCase;
+import org.eclipse.concierge.test.util.SyntheticBundleBuilder;
+import org.junit.Assert;
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+
+/**
+ * @author Jochen Hiller - Initial contribution
+ */
+public class FrameworkLaunchArgsTest extends AbstractConciergeTestCase {
+
+	/**
+	 * This test will install a bundle which refers to a class from Java runtime
+	 * (<code>javax.imageio</code>). As this <code>javax</code> package is
+	 * missing in system packages, it can NOT be used and an exception will be
+	 * thrown, which is expected behavior.
+	 */
+	@Test
+	public void testGetClassFromBootdelegationMissing() throws Exception {
+		try {
+			startFramework();
+			final SyntheticBundleBuilder builder = SyntheticBundleBuilder
+					.newBuilder();
+			builder.bundleSymbolicName("testGetClassFromBootdelegationMissing")
+					.addManifestHeader("Bundle-Version", "1.0.0");
+			final Bundle bundleUnderTest = installBundle(builder);
+			bundleUnderTest.start();
+			assertBundleResolved(bundleUnderTest);
+
+			final String className = "javax.imageio.ImageTranscoder";
+			final RunInClassLoader runner = new RunInClassLoader(
+					bundleUnderTest);
+			try {
+				runner.getClass(className);
+				Assert.fail("Oops, ClassNotFoundException expected");
+			} catch (ClassNotFoundException ex) {
+				// OK expected
+			}
+		} finally {
+			stopFramework();
+		}
+	}
+
+	/**
+	 * This test will install a bundle which refers to a class from Java runtime
+	 * (<code>javax.imageio</code>). As this <code>javax</code> package is added
+	 * to boot delegation, the class be used which is checked via reflection.
+	 */
+	@Test
+	public void testGetClassFromBootdelegationOK() throws Exception {
+		try {
+			final Map<String, String> launchArgs = new HashMap<String, String>();
+			launchArgs
+					.put("org.osgi.framework.bootdelegation", "javax.imageio");
+			startFrameworkClean(launchArgs);
+
+			final SyntheticBundleBuilder builder = SyntheticBundleBuilder
+					.newBuilder();
+			builder.bundleSymbolicName("testGetClassFromBootdelegationOK")
+					.addManifestHeader("Bundle-Version", "1.0.0");
+			final Bundle bundleUnderTest = installBundle(builder);
+			bundleUnderTest.start();
+			assertBundleResolved(bundleUnderTest);
+
+			final String className = "javax.imageio.ImageTranscoder";
+			final RunInClassLoader runner = new RunInClassLoader(
+					bundleUnderTest);
+			Class<?> clazz = runner.getClass(className);
+			Assert.assertNotNull(clazz);
+		} finally {
+			stopFramework();
+		}
+	}
+
+	/**
+	 * This test checks whether <code>system.packages</code> can be specified as
+	 * launcher args.
+	 */
+	@Test
+	public void testSystemPackages() throws Exception {
+		try {
+			final Map<String, String> launchArgs = new HashMap<String, String>();
+			launchArgs.put("org.osgi.framework.system.packages.extra",
+					"p1,p2,p3");
+			startFramework(launchArgs);
+		} finally {
+			stopFramework();
+		}
+	}
+
+	/**
+	 * This test will fail when property
+	 * <code>org.osgi.framework.system.packages</code> will contain a trailing
+	 * <code>,</code> (Comma).
+	 */
+	@Test
+	public void testSystemPackagesTrailingComma() throws Exception {
+		try {
+			final Map<String, String> launchArgs = new HashMap<String, String>();
+			launchArgs.put("org.osgi.framework.system.packages.extra",
+					"p1,p2,p3,");
+			startFramework(launchArgs);
+		} finally {
+			stopFramework();
+		}
+	}
+
+	/**
+	 * This test checks whether <code>system.packages.extra</code> can be
+	 * specified as launcher args.
+	 */
+	@Test
+	public void testSystemPackagesExtra() throws Exception {
+		try {
+			final Map<String, String> launchArgs = new HashMap<String, String>();
+			launchArgs.put("org.osgi.framework.system.packages.extra",
+					"p1,p2,p3");
+			startFramework(launchArgs);
+		} finally {
+			stopFramework();
+		}
+	}
+
+	/**
+	 * This test will fail when property
+	 * <code>org.osgi.framework.system.packages.extra</code> will contain a
+	 * trailing <code>,</code> (Comma).
+	 */
+	@Test
+	public void testSystemPackagesExtraTrailingComma() throws Exception {
+		try {
+			final Map<String, String> launchArgs = new HashMap<String, String>();
+			launchArgs.put("org.osgi.framework.system.packages.extra",
+					"p1,p2,p3,");
+			startFramework(launchArgs);
+		} finally {
+			stopFramework();
+		}
+	}
+}
