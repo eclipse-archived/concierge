c3b477aef78271b9fcb79e4be5f75410524cd2e4
Jochen Hiller <j.hiller@telekom.de>
2014-12-31 13:13:00 +0100

    Fixed problems in publish.sh
    Signed-off-by: Jochen Hiller <j.hiller@telekom.de>

--- distribution/publish/publish.sh
+++ distribution/publish/publish.sh
@@ -8,7 +8,6 @@ logFile=publish.log
 
 version=`cat version.txt`
 echo "VERSION=$version"
-if [ ]
 if [[ "$version" == *-SNAPSHOT ]] ; then
   BUILD_LOC_TYPE=snapshots
 else
@@ -27,12 +26,12 @@ now=`date '+%Y/%m/%d %H:%M:%S'`
 echo "$now: getting last successful build" >>$logFile
 
 wget --no-check-certificate https://hudson.eclipse.org/concierge/job/ConciergeDistribution/lastSuccessfulBuild/artifact/distribution/build/distributions/concierge-$version.zip -a $logFile
-wget --no-check-certificate https://hudson.eclipse.org/concierge/job/ConciergeDistribution/lastSuccessfulBuild/artifact/distribution/build/distributions/concierge-$version.tar -a $logFile
+wget --no-check-certificate https://hudson.eclipse.org/concierge/job/ConciergeDistribution/lastSuccessfulBuild/artifact/distribution/build/distributions/concierge-$version.tar.gz -a $logFile
 
 now=`date '+%Y/%m/%d %H:%M:%S'`
 echo "$now: publishing last successful build" >>$logFile
 
-mv -fv *.zip *.tar ../$BUILD_LOC_TYPE/ >>$logFile
+mv -fv *.zip *.tar.gz ../$BUILD_LOC_TYPE/ >>$logFile
 
 now=`date '+%Y/%m/%d %H:%M:%S'`
 echo "$now: finished publishing last successful build" >>$logFile
