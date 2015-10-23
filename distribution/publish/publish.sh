9c1b161b87d333600bb2616cb8b81a455939c371
Jochen Hiller <j.hiller@telekom.de>
2015-10-23 19:00:03 +0200

    Fixing now publishing of builds to download server
    Change-Id: I6cbd4c742f44b811bfb381470a650e024f416834
    Signed-off-by: Jochen Hiller <j.hiller@telekom.de>

--- distribution/publish/publish.sh
+++ distribution/publish/publish.sh
@@ -18,32 +18,24 @@ else
 fi
 echo "BUILD_LOC_TYPE=$BUILD_LOC_TYPE"
 
-pwd ; find .
-find /home/hudson/genie.concierge/.hudson/jobs/ConciergeDistribution/lastSuccessfulBuild
+# pwd ; find .
+# find /home/hudson/genie.concierge/.hudson/jobs/ConciergeDistribution/lastSuccessfulBuild
 
-
-
-if [ -d $BUILD_LOC/tmp ] ; then rm -rf $BUILD_LOC/tmp/* ; fi
-if [ ! -d $BUILD_LOC/tmp ] ; then mkdir -p $BUILD_LOC/tmp ; fi
-cd $BUILD_LOC/tmp
-
-rm -f $logFile
+# if [ -d $BUILD_LOC/tmp ] ; then rm -rf $BUILD_LOC/tmp/* ; fi
+# if [ ! -d $BUILD_LOC/tmp ] ; then mkdir -p $BUILD_LOC/tmp ; fi
 
 now=`date '+%Y/%m/%d %H:%M:%S'`
-echo "$now: getting last successful build" >>$logFile
+echo "$now: publishing last successful build" >>$BUILD_LOC/$logFile
 
-# wget --no-check-certificate https://hudson.eclipse.org/concierge/job/ConciergeDistribution/lastSuccessfulBuild/artifact/distribution/build/distributions/concierge-incubation-$version.zip -a $logFile
-# wget --no-check-certificate https://hudson.eclipse.org/concierge/job/ConciergeDistribution/lastSuccessfulBuild/artifact/distribution/build/distributions/concierge-incubation-$version.tar.gz -a $logFile
+cp ./distribution/build/distributions/*.zip $BUILD_LOC
+cp ./distribution/build/distributions/*.tar.gz $BUILD_LOC
 
-now=`date '+%Y/%m/%d %H:%M:%S'`
-echo "$now: publishing last successful build" >>$logFile
-
-mv -fv *.zip *.tar.gz ../$BUILD_LOC_TYPE/ >>$logFile
+# mv -fv *.zip *.tar.gz ../$BUILD_LOC_TYPE/ >>$logFile
 
 now=`date '+%Y/%m/%d %H:%M:%S'`
-echo "$now: finished publishing last successful build" >>$logFile
+echo "$now: finished publishing last successful build" >>$BUILD_LOC/$logFile
 
-mv -f $logFile ../$BUILD_LOC_TYPE/
+# mv -f $logFile ../$BUILD_LOC_TYPE/
 
-cd ..
+# cd ..
 if [ -d $BUILD_LOC/tmp ] ; then rm -rf $BUILD_LOC/tmp/* ; rmdir $BUILD_LOC/tmp ; fi
