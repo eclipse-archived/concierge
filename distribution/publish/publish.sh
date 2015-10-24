094dab8bd4f2f15b2ecb250650c466575053155e
Jochen Hiller <j.hiller@telekom.de>
2015-10-24 09:34:30 +0200

    Fixing now publishing of builds to download server
    Change-Id: Ibdda114e64f935a27cf06ee94b5969de6332247d
    Signed-off-by: Jochen Hiller <j.hiller@telekom.de>

--- distribution/publish/publish.sh
+++ distribution/publish/publish.sh
@@ -14,20 +14,23 @@ else
   BUILD_TYPE=releases
 fi
 echo "BUILD_TYPE=$BUILD_TYPE"
-UPLOAD_LOCATION=/home/data/httpd/download.eclipse.org/concierge/$BUILD_TYPE
-PUBLISH_LOG=$UPLOAD_LOCATION/publish.log
+UPLOAD_BASE=/home/data/httpd/download.eclipse.org/concierge
+UPLOAD_LOCATION=$UPLOAD_BASE/$BUILD_TYPE
+PUBLISH_LOG=$UPLOAD_BASE/publish.log
 echo "UPLOAD_LOCATION=$UPLOAD_LOCATION"
 echo "PUBLISH_LOG=$PUBLISH_LOG"
 
 now=`date '+%Y-%m-%d %H:%M:%S %Z'`
 echo "$now: publishing last successful build for $version" >>$PUBLISH_LOG
 
-echo -n "$BUILD_TYPE" >>$PUBLISH_LOG
+echo -n "$BUILD_TYPE/" >>$PUBLISH_LOG
 echo `(cd ./distribution/build/distributions/ ; ls *.zip)` >>$PUBLISH_LOG
 cp ./distribution/build/distributions/*.zip $UPLOAD_LOCATION
-echo -n "$BUILD_TYPE" >>$PUBLISH_LOG
+echo -n "$BUILD_TYPE/" >>$PUBLISH_LOG
 echo `(cd ./distribution/build/distributions/ ; ls *.tar.gz)` >>$PUBLISH_LOG
 cp ./distribution/build/distributions/*.tar.gz $UPLOAD_LOCATION
 
 # cleanup
-mv /home/data/httpd/download.eclipse.org/concierge/releases/concierge-incubation-5.0.0.M2.tar /home/data/httpd/download.eclipse.org/concierge/releases/concierge-incubation-5.0.0.M2.tar.gz
+rm /home/data/httpd/download.eclipse.org/concierge/releases/concierge-incubation-5.0.0.RC2.*
+rm /home/data/httpd/download.eclipse.org/concierge/releases/publish.log
+rm /home/data/httpd/download.eclipse.org/concierge/snapshots/publish.log
