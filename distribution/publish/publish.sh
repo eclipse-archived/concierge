800abc98270f568d910812093764c28b3d75bbaf
Jochen Hiller <j.hiller@telekom.de>
2015-10-24 08:32:28 +0200

    Fixing now publishing of builds to download server
    Change-Id: Ie35383dbce707b6576ae7a526594d1ff1b108b43
    Signed-off-by: Jochen Hiller <j.hiller@telekom.de>

--- distribution/publish/publish.sh
+++ distribution/publish/publish.sh
@@ -4,7 +4,7 @@
 # chmod u+x ./distribution/publish/publish.sh
 # ./distribution/publish/publish.sh
 
-set -x
+set-x
 
 version=`cat version.txt`
 echo "VERSION=$version"
@@ -28,7 +28,6 @@ echo `ls ./distribution/build/distributions/*.tar.gz` >>$PUBLISH_LOG
 cp ./distribution/build/distributions/*.tar.gz $UPLOAD_LOCATION
 
 # cleanup
-rm /home/data/httpd/download.eclipse.org/concierge/snapshots/concierge-0.8.0*
-rm /home/data/httpd/download.eclipse.org/concierge/snapshots/concierge-0.9.0*
-rm /home/data/httpd/download.eclipse.org/concierge/snapshots/concierge-1.0.0*
-rm /home/data/httpd/download.eclipse.org/concierge/snapshots/publish.log
+rm /home/data/httpd/download.eclipse.org/concierge/releases/publish.log
+rm /home/data/httpd/download.eclipse.org/concierge/publish.log
+mv /home/data/httpd/download.eclipse.org/concierge/concierge-incubation-5.0.0* /home/data/httpd/download.eclipse.org/concierge/releases
